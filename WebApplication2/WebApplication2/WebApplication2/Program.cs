667576e4c0f1ef801bf19a41e8c52c1018bfc44a
zaid Ahmad <khanzaid2490@gmail.com>
2023-08-10 22:27:20 +0530

    updated

---
+++ WebApplication2/WebApplication2/WebApplication2/Program.cs
@@ -0,0 +1,43 @@
+using Serilog;
+using Serilog.Sinks.Fluentd;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var log = new LoggerConfiguration().WriteTo.Fluentd("https://demo81aks-dns-c64dz6f2.hcp.eastus.azmk8s.io", 443).CreateLogger();
+#region Logs to Dynatrace 
+/// These are the lines
+//Log.Logger = new LoggerConfiguration().CreateBootstrapLogger();
+//builder.Host.UseSerilog(((ctx, lc) => lc
+
+//.ReadFrom.Configuration(ctx.Configuration)));
+#endregion
+
+var app = builder.Build();
+
+#region Final Line Dynatrace
+////
+///Final lines to make ILogger as logger component of Dyntrace
+//app.UseSerilogRequestLogging();
+
+#endregion
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
